358d53435d53f4870960715b8674c7f3984db3a0
Syed Daim <sd@ais.org>
2022-11-16 22:26:15 +0300

    test

---
+++ README.md
@@ -0,0 +1,17 @@
+# Past Papers For Real For Real
+##### ...brought to you by AIS' batch of 2023.
+
+
+## How to
+
+- Pick a subject
+- Pick a paper
+- ??????
+- Profit.
+
+## Features
+
+- Cambridge International A Level pastpapers
+
+
+Link to [website.](https://ppfrfr23.web.app/)
---
+++ public/__/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyACV5_helfNgqjuuzpEQymhQ9Y_AtQVc_s",
+  "appId": "1:26811607920:web:95d384c846efd027537db5",
+  "authDomain": "ppfrfr-68676.firebaseapp.com",
+  "databaseURL": "",
+  "measurementId": "G-2NS9467NZ4",
+  "messagingSenderId": "26811607920",
+  "projectId": "ppfrfr-68676",
+  "storageBucket": "ppfrfr-68676.appspot.com"
+});
