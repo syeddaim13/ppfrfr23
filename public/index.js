ad594755af0022a7a478d9a3574ead1c7f4f143b
Syed Daim <57717011+syeddaim13@users.noreply.github.com>
2022-11-20 14:57:26 +0300

    all papers available

--- public/index.js
+++ public/index.js
@@ -44,8 +44,12 @@ function run() {
             alert("Error in month switch. Please report this.");
     }
     console.log(subjectcode)
-
-    if (type == "sf" && subject != "IT") {
+// special cases
+    if (type = "sf" && subject == "IT" && year == "17" && monthcode == "m") {
+        window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+year+'_'+"SF"+'_'+paper+'.zip'; //link for sf 2017 march
+    }
+// end special cases
+    else if (type == "sf" && subject != "IT") {
         alert("uhh....are you sure you want SOURCE FILES? smh non IT ppl.")  //check if not IT & sf picked
     }
 
@@ -54,23 +58,35 @@ function run() {
         location.reload()
     }
 
+    else if (type == "sf" && year == "17") {
+        window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+paper+'.zip'; //link for sf 2017 june/nov
+    }
     else if (type == "sf") {
-        window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+paper+variant+'.zip'; //link for sf
+        window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+"0"+paper+'.zip'; //link for sf
     }
 
     else if (subject == "IT" && paper == "4" && type == "qp" && year == "17") {
         window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'4'+'.pdf';
         //link for IT p4 qp in 2017
     }
+    else if (subject == "IT" && paper == "2" && type == "qp" && year == "17") {
+        window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'2'+'.pdf';
+        //link for IT p2 qp in 2017
+    }
     else if (subject == "IT" && paper == "4" && type == "qp") {
         window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'04'+'.pdf';
         //link for IT p4 qp after 2017
     }
-
+    else if (subject == "IT" && paper == "2" && type == "qp") {
+        window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'02'+'.pdf';
+        //link for IT p2 qp after 2017
+    }
     else if (subject == "IT" && paper == "4" && type == "ms") {
         window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'4'+'.pdf'; //link for IT p4 ms
     }
-
+    else if (subject == "IT" && paper == "2" && type == "ms") {
+        window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'2'+'.pdf'; //link for IT p2 ms
+    }
     else if (monthcode == "m" && variant != "2") { //validate march variant
         alert("You want a March paper, yet picked a variant that is not equal to 2. This is shameful. We're redirecting you to variant 2 anyway.")
         window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+paper+'2'+'.pdf';
@@ -127,8 +143,12 @@ function newtab() {
             alert("Error in month switch. Please report this.");
     }
     console.log(subjectcode)
-
-    if (type == "sf" && subject != "IT") {
+// special cases
+    if (type = "sf" && subject == "IT" && year == "17" && monthcode == "m") {
+    window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+year+'_'+"SF"+'_'+paper+'.zip'; //link for sf 2017 march
+    }
+// end special cases
+    else if (type == "sf" && subject != "IT") {
         alert("uhh....are you sure you want SOURCE FILES? smh non IT ppl.")  //check if not IT & sf picked
     }
 
@@ -137,23 +157,37 @@ function newtab() {
         location.reload()
     }
 
-    else if (type == "sf") {
-        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+paper+variant+'.zip'); //link for sf
+    else if (type == "sf" && year == "17") {
+        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+paper+'.zip'); //link for sf 2017 june/nov
+    }
+
+    else if (type == "sf" && paper == "2" || paper == "4 ") {
+        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+"0"+paper+'.zip'); //link for sf
     }
 
     else if (subject == "IT" && paper == "4" && type == "qp" && year == "17") {
         window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'4'+'.pdf');
         //link for IT p4 qp in 2017
     }
+    else if (subject == "IT" && paper == "2" && type == "qp" && year == "17") {
+        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'2'+'.pdf');
+        //link for IT p2 qp in 2017
+    }
 
     else if (subject == "IT" && paper == "4" && type == "qp") {
         window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'04'+'.pdf'); //link for IT p4 qp
+    }
+    else if (subject == "IT" && paper == "2" && type == "qp") {
+        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'02'+'.pdf');
+        //link for IT p2 qp after 2017
     }
 
     else if (subject == "IT" && paper == "4" && type == "ms") {
         window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'4'+'.pdf'); //link for IT p4 ms
     }
-
+    else if (subject == "IT" && paper == "2" && type == "ms") {
+        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'2'+'.pdf'); //link for IT p2 ms
+    }
     else if (monthcode == "m" && variant != "2") { //validate march variant
         alert("You want a March paper, yet picked a variant that is not equal to 2. This is shameful. We're redirecting you to variant 2 anyway.")
         window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+paper+'2'+'.pdf');
