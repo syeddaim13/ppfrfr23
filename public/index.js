4f199eccf21538dc5330cfd8286b6a9bfeb1a34d
Syed Daim <57717011+syeddaim13@users.noreply.github.com>
2022-11-24 05:34:29 +0300

    Added support for AL Business

--- public/index.js
+++ public/index.js
@@ -5,6 +5,10 @@ function run() {
     year = document.getElementById("year").value
     type = document.getElementById("type").value
     variant = document.getElementById("variant").value
+    maxpapers = 5
+    papervalue = parseInt(paper)
+    yearvalue = parseInt(year)
+    sfeligible = false
 
     switch (subject) {
         case "Math":
@@ -14,6 +18,8 @@ function run() {
         case "IT":
             subjectcode = "9626"
             subjectlink = "Information%20Technology%20(9626)"
+            maxpapers = 4
+            sfeligible = true
             break;
         case "Chemistry":
             subjectcode = "9701"
@@ -26,6 +32,12 @@ function run() {
         case "Biology":
             subjectcode = "9700"
             subjectlink = "Biology%20(9700)"
+            break;
+        case "Business":
+            subjectcode = "9609"
+            subjectlink = "Business%20(9609)"
+            maxpapers = 3
+            sfeligible = true 
             break;
         default:
             alert("Error in switch. Please report this.");
@@ -45,7 +57,7 @@ function run() {
     }
     console.log(subjectcode)
 //paper validation
-    if (subject == "IT" && paper == "5") {
+    if (papervalue > maxpapers) {
         alert("The chosen subject does not have a paper pertaining to this value.")
     }
     if (year == "22" && monthcode == "w") {
@@ -57,8 +69,10 @@ function run() {
         window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+year+'_'+"SF"+'_'+paper+'.zip'; //link for sf 2017 march
     }
 // end special cases
-    else if (type == "sf" && subject != "IT") {
-        alert("uhh....are you sure you want SOURCE FILES? smh non IT ppl.")  //check if not IT & sf picked
+
+    //IT
+    else if (type == "sf" && sfeligible == false ) {
+        alert("Source files and/or insert files aren't available for "+subject+".")  //validate sf subject
     }
 
     else if (subject == "IT" && monthcode == "m" && variant != "2") {
@@ -66,7 +80,7 @@ function run() {
         location.reload()
     }
 
-    else if (type == "sf") {
+    else if (type == "sf" && subject == "IT") {
         window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+paper+variant+'.zip'; //link for sf
     }
 
@@ -82,6 +96,26 @@ function run() {
     else if (subject == "IT" && paper == "4" && type == "ms") {
         window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'4'+'.pdf'; //link for IT p4 ms
     }
+    //IT END
+
+    //Business
+    else if(subject == "Business" && paper != "3" && type == "sf") {
+        alert("Inserts are only available for paper 3.")
+    }
+    
+    else if(subject == "Business" && paper == "3" && type == "sf" && yearvalue <= 17) {
+        alert("Inserts weren't available in this year.")
+    }
+    
+    else if (subject == "Business" && paper == "3" && type == "sf" && monthcode == "m" && variant != '2') {
+        alert("Please pick variant 2.")
+    }
+
+    else if (subject == "Business" && paper == "3" && type == "sf") {
+        window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+'in'+'_'+paper+variant+'.pdf';
+
+    }
+    //Business END
 
     else if (monthcode == "m" && variant != "2") { //validate march variant
         alert("You want a March paper, yet picked a variant that is not equal to 2. This is shameful. We're redirecting you to variant 2 anyway.")
@@ -100,6 +134,10 @@ function newtab() {
     year = document.getElementById("year").value
     type = document.getElementById("type").value
     variant = document.getElementById("variant").value
+    maxpapers = 5
+    papervalue = parseInt(paper)
+    yearvalue = parseInt(year)
+    sfeligible = false
 
     switch (subject) {
         case "Math":
@@ -109,6 +147,8 @@ function newtab() {
         case "IT":
             subjectcode = "9626"
             subjectlink = "Information%20Technology%20(9626)"
+            maxpapers = 4
+            sfeligible = true
             break;
         case "Chemistry":
             subjectcode = "9701"
@@ -121,6 +161,12 @@ function newtab() {
         case "Biology":
             subjectcode = "9700"
             subjectlink = "Biology%20(9700)"
+            break;
+        case "Business":
+            subjectcode = "9609"
+            subjectlink = "Business%20(9609)"
+            maxpapers = 3
+            sfeligible = true 
             break;
         default:
             alert("Error in switch. Please report this.");
@@ -140,7 +186,7 @@ function newtab() {
     }
     console.log(subjectcode)
 //paper validation
-    if (subject == "IT" && paper == "5") {
+    if (papervalue > maxpapers) {
         alert("The chosen subject does not have a paper pertaining to this value.")
     }
     if (year == "22" && monthcode == "w") {
@@ -149,11 +195,13 @@ function newtab() {
 //paper validation end
     // special cases
     else if (type == "sf" && subject == "IT" && year == "17" && monthcode == "m") {
-    window.location.href = 'https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+year+'_'+"SF"+'_'+paper+'.zip'; //link for sf 2017 march
+        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+year+'_'+"SF"+'_'+paper+'.zip'); //link for sf 2017 march
     }
 // end special cases
-    else if (type == "sf" && subject != "IT") {
-        alert("uhh....are you sure you want SOURCE FILES? smh non IT ppl.")  //check if not IT & sf picked
+
+    //IT
+    else if (type == "sf" && sfeligible == false ) {
+        alert("Source files and/or insert files aren't available for "+subject+".")  //validate sf subject
     }
 
     else if (subject == "IT" && monthcode == "m" && variant != "2") {
@@ -161,7 +209,7 @@ function newtab() {
         location.reload()
     }
 
-    else if (type == "sf") {
+    else if (type == "sf" && subject == "IT") {
         window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+paper+variant+'.zip'); //link for sf
     }
 
@@ -169,14 +217,34 @@ function newtab() {
         window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'4'+'.pdf');
         //link for IT p4 qp in 2017
     }
-
     else if (subject == "IT" && paper == "4" && type == "qp") {
-        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'04'+'.pdf'); //link for IT p4 qp
+        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'04'+'.pdf');
+        //link for IT p4 qp after 2017
     }
 
     else if (subject == "IT" && paper == "4" && type == "ms") {
         window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+type+'_'+'4'+'.pdf'); //link for IT p4 ms
     }
+    //IT END
+
+    //Business
+    else if(subject == "Business" && paper != "3" && type == "sf") {
+        alert("Inserts are only available for paper 3.")
+    }
+    
+    else if(subject == "Business" && paper == "3" && type == "sf" && yearvalue <= 17) {
+        alert("Inserts weren't available in this year.")
+    }
+    
+    else if (subject == "Business" && paper == "3" && type == "sf" && monthcode == "m" && variant != '2') {
+        alert("Please pick variant 2.")
+    }
+
+    else if (subject == "Business" && paper == "3" && type == "sf") {
+        window.open('https://papers.gceguide.com/A%20Levels/'+subjectlink+'/20'+year+'/'+subjectcode+'_'+monthcode+year+'_'+'in'+'_'+paper+variant+'.pdf');
+
+    }
+    //Business END
 
     else if (monthcode == "m" && variant != "2") { //validate march variant
         alert("You want a March paper, yet picked a variant that is not equal to 2. This is shameful. We're redirecting you to variant 2 anyway.")
